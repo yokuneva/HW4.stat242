eca7cafddf40f090354fd5feb25c3049b8cd65f3
yokuneva <ysokuneva@ucdavis.edu>
2013-03-20 10:40:39 -0700

    HW5 code

---
+++ HW5.code.r
@@ -0,0 +1,154 @@
+
+
+
+setwd("/home/jenya/hw6")
+
+##################################################create a list of 22 file names
+data=list.files()
+
+
+###################################################get sample via lapply
+
+getSample = function(listOfFiles){do.call("rbind",lapply(listOfFiles, function (x){
+  
+  k=read.csv(x)
+  set.seed(1985)
+  k[sample(1:nrow(k), 100000, replace = FALSE),]
+}))
+}
+
+ourSample = getSample(data)
+
+system.time(getSample(data))
+
+
+################################################substitute lapply with mclapply
+
+RNGkind("L'Ecuyer-CMRG")
+set.seed(1985)
+mc.cores = detectCores()
+
+results = mclapply(data,  function (x){
+  
+  k=read.csv(x)
+  i = sample(1:nrow(k), 100000, replace = FALSE)
+  k = k[i,]
+}, mc.cores = mc.cores)
+
+
+
+
+
+
+
+
+################################################parallelize the above with parLapplyLB
+RNGkind("L'Ecuyer-CMRG")
+cl = makeCluster(detectCores(), type = "FORK")
+clusterSetRNGStream(cl, 1985)
+
+results = parLapplyLB(cl, list.files(), function (x){
+  
+  k=read.csv(x)
+  k[sample(1:nrow(k), 100000, replace = FALSE),]
+})
+
+
+stopCluster(cl)
+###############################################################do the above with clusterApplyLB
+RNGkind("L'Ecuyer-CMRG")
+
+cl = makeCluster(detectCores(), type = "FORK")
+clusterSetRNGStream(cl, 1985)
+
+data=list.files()
+results = clusterApplyLB(cl, data, function (x){
+  
+  k=read.csv(x)
+  k[sample(1:nrow(k), 100000, replace = FALSE),]
+})
+
+stopCluster(cl)
+
+
+
+######read data into R and then sample getting 100000 obs per year with parSapply
+
+
+getData = do.call("rbind", lapply(data, read.csv))
+
+system.time(do.call("rbind", lapply(data, read.csv)))
+
+RNGkind("L'Ecuyer-CMRG")
+set.seed(1985)
+cl = makeCluster(4, type = "FORK")
+
+results = parSapply(cl, getData$year, function(x) {
+  x[sample(1:nrow(x), 100000, replace = FALSE),]
+})
+
+
+stopCluster(cl)
+
+
+####################################################################
+sapply(ourSample, function (x) length(unique(x)))
+sapply(ourSample, class)
+
+ourSample$DayOfWeek = factor(ourSample$DayOfWeek)
+
+
+
+
+
+RNGkind("L'Ecuyer-CMRG")
+
+cl = makeCluster(detectCores(), type = "FORK")
+clusterSetRNGStream(cl, 1985)
+
+randomFor = function (x,...){
+  
+  k =randomForest(ArrDelay ~ DayOfWeek + DepDelay + Distance,
+                  replace = TRUE, 
+                  data = x, ntree=10, na.action = na.omit)
+}
+
+results = clusterCall(cl,ls , , ourSample)
+
+
+stopCluster(cl)
+
+
+
+
+RNGkind("L'Ecuyer-CMRG")
+
+cl = makeCluster(detectCores(), type = "FORK")
+clusterSetRNGStream(cl, 1985)
+clusterEvalQ(cl, ls())
+
+results = clusterCall(cl, function(...) { replicate(5, {
+  
+  k =randomForest(ArrDelay ~ DayOfWeek + DepDelay + Distance,
+                  replace = TRUE, 
+                  data = ourSample, ntree=10, na.action = na.omit)
+})})
+
+stopCluster(cl)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
